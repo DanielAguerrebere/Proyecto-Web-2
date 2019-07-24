c58aa8e947a6e84b6f093aa74302a1724e782c55
DanielAguerrebere <Daniel_aguerrebere@hotmail.com>
2019-07-24 18:42:43 -0500

    test

--- PAYPAGE/TEST/FIRST.php
+++ PAYPAGE/TEST/FIRST.php
@@ -6,7 +6,7 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Card Elements Test</title>
-    <link rel="stylesheet" href="CSS/style.css">
+    <link rel="stylesheet" href="SECOND.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
 </head>
@@ -35,6 +35,6 @@
 
     </form>
 </div>
-<script src="JS/charge.js"></script>
+<script src="THIRD.js"></script>
 </body>
 </html>
